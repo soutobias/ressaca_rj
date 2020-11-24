8d1c75bc85f1a85bd1b2f5f931e6d18990263aba
vanessabach <vanessabach.r@gmail.com>
2020-11-24 16:00:01 -0300

    skip autentication

--- app/controllers/dados_controller.rb
+++ app/controllers/dados_controller.rb
@@ -1,28 +1,8 @@
 class DadosController < ApplicationController
-  before_action :api_spotter, only: [:index]
+  skip_before_action :authenticate_user!, only: [ :index ]
 
 	def index
     @values = api_spotter()
-
-
-    @data = {
-      labels: @values[:timestamp],
-      datasets: [
-        {
-            label: "My First dataset",
-            backgroundColor: "rgba(220,220,220,0.2)",
-            borderColor: "rgba(220,220,220,1)",
-            data: @values[:significantwaveheight]
-        },
-        {
-            label: "My Second dataset",
-            backgroundColor: "rgba(151,187,205,0.2)",
-            borderColor: "rgba(151,187,205,1)",
-            data: @values[:meanperiod]
-        }
-      ]
-    }
-
 	end
 
   private
